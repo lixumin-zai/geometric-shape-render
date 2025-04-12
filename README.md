56a223f4fef1f13901509d1ed9e15ad7f2a56525
lismin <lixumin1030@gmail.com>
2025-04-12 23:19:08 +0800

    feat: first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# geometry-shape-renderer
+This is a simple program that renders a 2D shape to the screen.
+
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        123
+      </div>
+    </>
+  )
+}
+
+export default App
